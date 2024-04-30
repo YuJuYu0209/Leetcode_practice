7fe61359e45ded546ccc7c0d413b942599fd56b7
YuJuYu0209 <157779433+YuJuYu0209@users.noreply.github.com>
2024-04-30 15:07:15 +0800

    Time: 0 ms (100%), Space: 7.4 MB (12.34%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> num(n+1);
+        num[0] = 1;
+        num[1] = 1;
+        for(int i = 2; i <= n  ;i++)
+        {
+            num[i] = num[i-1] + num[i-2];
+        }
+        return num[n];
+    }
+};
