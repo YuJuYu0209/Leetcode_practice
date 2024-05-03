8340ae56a8cada821861d8fb38fc7d18f2ed05a3
YuJuYu0209 <157779433+YuJuYu0209@users.noreply.github.com>
2024-05-03 13:42:55 +0800

    Time: 34 ms (12.87%), Space: 22.3 MB (90.47%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
@@ -4,11 +4,11 @@ public:
         int ans = -1;
         for(int i = 0; i < nums.size(); i++)
         {
-            for(int j = 0; j < nums.size(); j++)
+            for(int j = i+1 ; j < nums.size(); j++)
             {
-                if(nums[i] == -nums[j] && nums[i] > ans)
+                if(nums[i] == -nums[j] && abs(nums[i]) > ans)
                 {
-                    ans = nums[i];
+                    ans = abs(nums[i]);
                 }
             }
         }
